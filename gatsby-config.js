093b707837191becd00f512bfa1c246765627c0d
Alexandrshy <alexandrshy@gmail.com>
2019-12-04 17:57:53 +0400

    Init project

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Alexandr Shulaev",
+    siteUrl: "",
+    description: "Personal Portfolio Website",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: "",
+        accessToken: "",
+      },
+    },
+    "gatsby-plugin-typescript",
+  ],
+}
