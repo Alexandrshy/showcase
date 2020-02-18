113330de16a3d2ee10ea066b3bf498e5b6c21f34
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 08:15:31 +0400

    Fixed metadata

--- src/hooks/use-site-metadata.ts
+++ src/hooks/use-site-metadata.ts
@@ -15,7 +15,6 @@ export default function useSiteMetadata() {
           }
           author {
             name
-            photo
             contacts {
               email
               telegram
