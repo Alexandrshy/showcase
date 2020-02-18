aeedff6c2485445c905653ec6a3921332bac662a
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 08:14:57 +0400

    Redesigned footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,14 +1,32 @@
 import React from "react"
 
 import { Social } from "../social/social"
+import { Wrapper } from "../../components/wrapper/wrapper"
 
 import style from "./footer.module.css"
 
 export const Footer: React.FC = () => (
   <footer className={style.footer}>
-    <h4 className={style.title}>
-      Would you like to contact me for any question?
-    </h4>
-    <Social isCenter={true} />
+    <Wrapper>
+      <div className={style.box}>
+        <div className={style.author}>
+          <h5 className={style.title}>Alex Shulaev</h5>
+          <p className={style.text}>
+            Frontend developer at{" "}
+            <a
+              href="https://www.glassesusa.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GlassesUSA.com
+            </a>
+          </p>
+        </div>
+        <div className={style.social}>
+          <h5 className={style.title}>Contact me on:</h5>
+          <Social />
+        </div>
+      </div>
+    </Wrapper>
   </footer>
 )
