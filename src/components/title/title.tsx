ac96f09069fed1bae9c54e32fcf837ff09e2e261
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 08:24:08 +0400

    Changed styles

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,11 +1,19 @@
 import React from "react"
+import classNames from "classnames"
 
 import style from "./title.module.css"
 
 type PropsType = {
   title?: string
+  inline?: boolean
 }
 
-export const Title: React.FC<PropsType> = ({ title }) => (
-  <>{title && <h1 className={style.title}>{title}</h1>}</>
+export const Title: React.FC<PropsType> = ({ title, inline }) => (
+  <>
+    {title && (
+      <h1 className={classNames(style.title, { [style.inline]: inline })}>
+        {title}
+      </h1>
+    )}
+  </>
 )
