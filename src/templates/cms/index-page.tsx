6600f99c008e5b246624ded94fde1af0cb145d18
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 08:24:36 +0400

    Fixed templates

--- src/templates/cms/index-page.tsx
+++ src/templates/cms/index-page.tsx
@@ -1,16 +1,14 @@
 import React from "react"
 
-import style from "./about-page.module.css"
-
 type PropsType = {
   title: string
   content: Node
 }
 
 export const IndexPageTemplate: React.FC<PropsType> = ({ title, content }) => (
-  <section className={style.section}>
-    <div className={style.wrapper}>
-      <h2 className={style.title}>{title}</h2>
+  <section>
+    <div>
+      <h2>{title}</h2>
       {content}
     </div>
   </section>
