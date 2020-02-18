097061869ef34860e50cfe77b29bc4202f23dc29
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 07:58:40 +0400

    Added pagination

---
+++ src/components/pagination/pagination.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "gatsby"
+import classNames from "classnames"
+
+import style from "./pagination.module.css"
+
+type PropsType = {
+  prevPagePath: string
+  nextPagePath: string
+  hasPrevPage: boolean
+  hasNextPage: boolean
+}
+
+export const Pagination: React.FC<PropsType> = ({
+  prevPagePath,
+  nextPagePath,
+  hasPrevPage,
+  hasNextPage,
+}) => (
+  <nav className={style.wrapper}>
+    <Link
+      rel="prev"
+      to={prevPagePath}
+      className={classNames(style.link, style.prev, {
+        [style.inactive]: !hasPrevPage,
+      })}
+    >
+      Prev
+    </Link>
+    <Link
+      rel="next"
+      to={nextPagePath}
+      className={classNames(style.link, style.next, {
+        [style.inactive]: !hasNextPage,
+      })}
+    >
+      Next
+    </Link>
+  </nav>
+)
