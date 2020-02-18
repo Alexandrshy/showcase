dfafa15b2eeb1e2065610ac9b04460e3b5cc6e99
Alexandrshy <alexandrshy@gmail.com>
2020-02-18 08:12:32 +0400

    Removed blog from CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,7 @@
 import CMS from "netlify-cms-app"
 
 import AboutPagePreview from "./templates/about"
-import BlogPostPreview from "./templates/blog"
 import IndexPagePreview from "./templates/index"
 
 CMS.CMS.registerPreviewTemplate("index", IndexPagePreview)
 CMS.CMS.registerPreviewTemplate("about", AboutPagePreview)
-CMS.CMS.registerPreviewTemplate("blog", BlogPostPreview)
