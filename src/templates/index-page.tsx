2075c05c3d730db8df87e7b110534c8e4701a24e
Alexandrshy <alexandrshy@gmail.com>
2020-02-21 00:19:41 +0400

    Small fixed for index template

--- src/templates/index-page.tsx
+++ src/templates/index-page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import LatestPosts from "../components/leatestPosts/leatestPosts"
 import { Layout } from "../components/layout/layout"
 import { Intro } from "../components/intro/intro"
 import { Wrapper } from "../components/wrapper/wrapper"
+
+import style from "./index-page.module.css"
 
 type PropsType = {
   data: {
@@ -18,6 +20,7 @@ type PropsType = {
 const MainPage: React.FC<PropsType> = ({ data }) => {
   const {
     markdownRemark: {
+      html,
       frontmatter: { title, subtitle },
     },
   } = data
@@ -25,8 +28,13 @@ const MainPage: React.FC<PropsType> = ({ data }) => {
   return (
     <Layout>
       <Wrapper>
-        <Intro title={title} subtitle={subtitle} />
+        <Intro title={title} subtitle={html} />
         <LatestPosts />
+        <div className={style.box}>
+          <Link to="/posts" className={style.link}>
+            Read all posts
+          </Link>
+        </div>
       </Wrapper>
     </Layout>
   )
@@ -37,6 +45,7 @@ export default MainPage
 export const mainPageQuery = graphql`
   query MainPage {
     markdownRemark(frontmatter: { template: { eq: "index-page" } }) {
+      html
       frontmatter {
         title
         subtitle
