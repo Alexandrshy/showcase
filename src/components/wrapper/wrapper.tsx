d4c3dc8f0768580663aab8e69f7d42597c552e7a
Alexandrshy <alexandrshy@gmail.com>
2020-02-21 00:23:20 +0400

    Added new props

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -5,11 +5,18 @@ import style from "./wrapper.module.css"
 
 type PropsType = {
   width?: "normal" | "increased"
+  className?: string
   children: ReactNode
 }
 
-export const Wrapper: React.FC<PropsType> = ({ width, children }) => (
-  <div className={classNames(style[width], style.wrapper)}>{children}</div>
+export const Wrapper: React.FC<PropsType> = ({
+  width,
+  className,
+  children,
+}) => (
+  <div className={classNames(style[width], style.wrapper, className)}>
+    {children}
+  </div>
 )
 
 Wrapper.defaultProps = {
