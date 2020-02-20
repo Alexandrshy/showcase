4e16b060b50c337442879cbe57a48605be0c4f49
Alexandrshy <alexandrshy@gmail.com>
2020-02-21 00:15:01 +0400

    Small fixed for post template

--- src/templates/post-template.tsx
+++ src/templates/post-template.tsx
@@ -6,11 +6,13 @@ import { Layout } from "../components/layout/layout"
 import { Page } from "../components/page/page"
 import { Wrapper } from "../components/wrapper/wrapper"
 
+import style from "./post-page.module.css"
+
 type PropsType = {
   data: {
     markdownRemark: {
       html: string
-      frontmatter: { date: string; title: string; tags: string }
+      frontmatter: { date: string; title: string; tags: Array<string> }
     }
   }
 }
@@ -26,8 +28,35 @@ const BlogTemplate: React.FC<PropsType> = ({ data }) => {
     <Layout title={title}>
       <Page>
         <Wrapper>
-          <Title title={title} />
-          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <article className={style.article}>
+            <header>
+              <Title title={title} />
+            </header>
+            <div className={style.translation}>
+              <p>
+                This article has a <a>Russian</a> translation
+              </p>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <hr className={style.line} />
+            <footer>
+              <a
+                className={style.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Discuss on Twitter
+              </a>
+              {` • `}
+              <a
+                className={style.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Edit on GitHub
+              </a>
+            </footer>
+          </article>
         </Wrapper>
       </Page>
     </Layout>
