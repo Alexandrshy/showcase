5b3d2a442a98cb39c5cc131efa98cf35ff0da283
Alexandrshy <alexandrshy@gmail.com>
2020-02-23 19:17:01 +0400

    Moved button to leatestPosts component

--- src/components/leatestPosts/leatestPosts.tsx
+++ src/components/leatestPosts/leatestPosts.tsx
@@ -1,8 +1,10 @@
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import { graphql, StaticQuery, Link } from "gatsby"
 
 import { PropType as PostTypeType } from "../posts/post/post"
 import { Posts } from "../posts/posts"
+
+import style from "./leatestPosts.module.css"
 
 type PropsType = {
   data: {
@@ -24,14 +26,28 @@ const LatestPosts: React.FC<PropsType> = ({
   data: {
     allMarkdownRemark: { edges = [] },
   },
-}) => edges.length > 0 && <Posts list={edges} title="My last posts" />
+}) => (
+  <>
+    {edges.length > 0 && <Posts list={edges} title="My last posts" />}
+    {edges.length === 5 && (
+      <div className={style.box}>
+        <Link to="/posts" className={style.link}>
+          Read all posts
+        </Link>
+      </div>
+    )}
+  </>
+)
 
 export default () => (
   <StaticQuery
     query={graphql`
       query PostListTemplate {
         allMarkdownRemark(
-          filter: { frontmatter: { template: { eq: "post" } } }
+          filter: {
+            frontmatter: { template: { eq: "post" } }
+            fields: { langKey: { eq: "en" } }
+          }
           limit: 5
         ) {
           edges {
